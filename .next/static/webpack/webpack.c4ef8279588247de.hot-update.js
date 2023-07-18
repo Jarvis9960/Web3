65c9cf6ece252779843bbcbb0adfe1b2a93ebf87
Jarvis9960 <ankitfukte11@gmail.com>
2023-07-18 18:22:45 +0530

    finishing up the project

--- .next/static/webpack/webpack.c4ef8279588247de.hot-update.js
+++ .next/static/webpack/webpack.c4ef8279588247de.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "9655a3b74554bd04"; }
+/******/ 	__webpack_require__.h = function() { return "9fbf4dcc4927122d"; }
 /******/ }();
 /******/ 
 /******/ }
